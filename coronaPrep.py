e033356301fb7b8934554fc21b359323977adb8a
Michael Robbins <tuk36101@temple.edu>
2020-03-23 15:10:25 -0400

    my second commit

--- coronaPrep.py
+++ coronaPrep.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import, division, print_function
 import pandas as pd
+import geopandas as gpd
+import os
 
+os.chdir(r'C:\coronaMaps')
 csv = r'https://disasterresponse.maps.arcgis.com/sharing/rest/content/items/6f3f214220f443b2beed8d1374b02cf7/data'
 df = pd.read_csv(csv)
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Long, df.Lat))
+gdf.to_file("output.json", driver="GeoJSON")
